951a84a7158055364f160f202428b1aa9350e389
ferron <ferron@163.com>
2026-09-24 02:24:44 +0800

    feat: 将数据获取移入组件内部，添加suspense，给card分组

--- app/dashboard/(overview)/page.tsx
+++ app/dashboard/(overview)/page.tsx
@@ -1,28 +1,39 @@
-import { Card } from '@/app/ui/dashboard/cards';
+import CardWrapper, { Card } from '@/app/ui/dashboard/cards';
 import RevenueChart from '@/app/ui/dashboard/revenue-chart';
 import LatestInvoices from '@/app/ui/dashboard/latest-invoices';
 import { lusitana } from '@/app/ui/fonts';
-import {fetchRevenue,fetchLatestInvoices,fetchCardData} from '@/app/lib/data';//数据库获取
+import {fetchCardData} from '@/app/lib/data';//数据库获取
+import { Suspense } from 'react';
+import { RevenueChartSkeleton,LatestInvoicesSkeleton, CardsSkeleton } from '@/app/ui/skeletons';
 
 
 export default async function Page() {
-  const revenue = await fetchRevenue();
-  const latestInvoices = await fetchLatestInvoices();
-  const {totalPaidInvoices,totalPendingInvoices,numberOfInvoices,numberOfCustomers} = await fetchCardData();
+  // const revenue = await fetchRevenue(); // steaming a component
+  // 如果某个组件每次调用都需要指定数据来源，也很麻烦，将数据获取逻辑抽象至组件内部。
+  // const latestInvoices = await fetchLatestInvoices();
+  // const {totalPaidInvoices,totalPendingInvoices,numberOfInvoices,numberOfCustomers} = await fetchCardData();
   return (
     <main>
       <h1 className={`${lusitana.className} mb-4 text-xl md:text-2xl`}>
         数据面板
       </h1>
       <div className="grid gap-6 sm:grid-cols-2 lg:grid-cols-4">
-        <Card title="已收取发票" value={totalPaidInvoices} type="collected" />
+        {/* <Card title="已收取发票" value={totalPaidInvoices} type="collected" />
         <Card title="待收取发票" value={totalPendingInvoices} type="pending" />
         <Card title="发票总数" value={numberOfInvoices} type="invoices" />
-        <Card title="客户总数" value={numberOfCustomers} type="customers" />
+        <Card title="客户总数" value={numberOfCustomers} type="customers" /> */}
+        <Suspense fallback={<CardsSkeleton/>}>
+          <CardWrapper/>
+        </Suspense>
       </div>
       <div className="mt-6 grid grid-cols-1 gap-6 md:grid-cols-4 lg:grid-cols-8">
-        <RevenueChart revenue={revenue}  />
-        <LatestInvoices latestInvoices={latestInvoices} />
+        {/* 思考 suspense 和 fallback 也可以移动到组件内部 */}
+        <Suspense fallback={<RevenueChartSkeleton/>}>
+          <RevenueChart/>
+        </Suspense>
+        <Suspense fallback={<LatestInvoicesSkeleton/>}>
+          <LatestInvoices />
+        </Suspense>
       </div>
     </main>
   );
--- app/ui/dashboard/revenue-chart.tsx
+++ app/ui/dashboard/revenue-chart.tsx
@@ -2,6 +2,8 @@ import { generateYAxis } from '@/app/lib/utils';
 import { CalendarIcon } from '@heroicons/react/24/outline';
 import { lusitana } from '@/app/ui/fonts';
 import { Revenue } from '@/app/lib/definitions';
+import { fetchRevenue } from '@/app/lib/data';
+
 
 // This component is representational only.
 // For data visualization UI, check out:
@@ -9,11 +11,15 @@ import { Revenue } from '@/app/lib/definitions';
 // https://www.chartjs.org/
 // https://airbnb.io/visx/
 
-export default async function RevenueChart({
-  revenue,
-}: {
-  revenue: Revenue[];
-}) {
+// export default async function RevenueChart({
+//   revenue,
+// }: {
+//   revenue: Revenue[];
+// }) 
+export default async function RevenueChart(){ //移除原来的props，改为组件内直接获取。
+
+  const revenue:Revenue[] = await fetchRevenue(); //数据获取逻辑移动到组件内部。
+
   const chartHeight = 350;
   // NOTE: Uncomment this code in Chapter 7
 
